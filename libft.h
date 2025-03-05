c59d1e7d726247c15ca75a4bbe135cc2d24d3c93
mvlrose <143405213+mvlrose@users.noreply.github.com>
2025-03-05 04:21:39 +0100

    [ADD] ft_bzero

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: melrose <melrose@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 17:25:40 by melrose           #+#    #+#             */
-/*   Updated: 2025/03/05 04:15:19 by melrose          ###   ########.fr       */
+/*   Updated: 2025/03/05 04:20:36 by melrose          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+void	ft_bzero(void *b, size_t len);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
